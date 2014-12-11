d4237371f50bbfacb75b7cbf10b3b6d00d97dc20
Jan De Wilde <jan.dewilde@kbc.be>
2014-12-11 17:13:22 +0100

    Added src and dist folder + index.html

---
+++ Examples/Frontend Build Tools/demo/src/core.js
@@ -0,0 +1,4 @@
+var myFunc = function() {
+    var x = 100;
+    alert(x);
+};
---
+++ Examples/Frontend Build Tools/demo/src/someScript/someScript.js
@@ -0,0 +1,6 @@
+var someVariable = 'Check this out!';
+var someOtherVariable = 'Check this out also!';
+
+var someScriptFunc = function() {
+    alert(someVariable);
+};
